0d18ffba6cc63a3cce9ca28d2569daf49849b87f
Harsh Pandey <hpandey793@gmail.com>
2015-01-02 03:27:40 -0500

    add iron-router dep

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'hpx7:iron-router-search',
   summary: 'Simple search functionality with Meteor and iron:router',
-  version: '0.0.1',
+  version: '0.1.0',
   git: 'https://github.com/hpx7/meteor-iron-router-search.git'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0.2');
   api.use(['underscore', 'jquery', 'reactive-var'], 'client');
+  api.use('iron:router@1.0.0', 'client');
   api.addFiles('search.js', 'client');
   api.export('MeteorSearch', 'client');
 });
