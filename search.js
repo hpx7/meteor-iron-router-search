75536999b710536547a4e7cbc44e06be425bb18c
Harsh Pandey <hpandey793@gmail.com>
2015-01-21 10:02:15 -0500

    simplify package

--- search.js
+++ search.js
@@ -1,35 +1,14 @@
 MeteorSearch = function (options) {
+  options = options || {};
   var searchTemplate = options.searchTemplate || Template.search;
-  var resultsTemplate = options.resultsTemplate || searchTemplate;
   var inputSelector = options.inputSelector || '.queryinput';
   var urlKey = options.urlKey || 'q';
-  var searchFn = options.searchFn || _.identity;
 
   searchTemplate.helpers({
     query: function () {
       return Router.current().params.query[urlKey];
     }
   });
-
-  resultsTemplate.helpers({
-    searchResults: function () {
-      return Template.instance().results.get();
-    }
-  });
-
-  resultsTemplate.created = function () {
-    var self = this;
-    self.results = new ReactiveVar([]);
-    self.autorun(function () {
-      var query = Router.current() && Router.current().params.query && Router.current().params.query[urlKey];
-      if (query) {
-        searchFn(query, function (error, data) {
-          self.results.set(data);
-        })
-      } else
-        self.results.set([]);
-    });
-  };
 
   searchTemplate.events({
     'submit form': function (e) {
@@ -40,4 +19,10 @@ MeteorSearch = function (options) {
       return false;
     }
   });
-}
+
+  return {
+    getSearchQuery: function () {
+      return Router.current() && Router.current().params.query && Router.current().params.query[urlKey];
+    }
+  };
+};
